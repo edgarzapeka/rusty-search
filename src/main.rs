151fad93e2363ba9d915837524aec266cb10478e
Edgar Zapeka <edgar.zapeka@gmail.com>
2020-04-13 16:28:09 -0700

    add regex support

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-fn main() {
-    let search_term = "picture";
-    let quote = "Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books. What do we seek through millions of pages?";
+extern crate regex;
 
-    for (idx, line) in quote.lines().enumerate() {
-        if line.contains(search_term) {
-            println!("{}: {}", idx + 1, line);
+use regex::Regex;
+
+fn main() {
+    let re = Regex::new("picture").unwrap();
+
+    let quote = "Every face, every shop,
+bedroom window, public-house, and
+dark square is a picture
+feverishly turned--in search of what?
+It is the same with books.
+What do we seek
+through millions of pages?";
+
+    for line in quote.lines() {
+        match re.find(line) {
+            Some(_) => println!("{}", line),
+            None => (),
         }
     }
 }
