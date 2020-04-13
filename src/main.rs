46d1adfea21f13b9463c93d55e61582fd3d7ad32
Edgar Zapeka <edgar.zapeka@gmail.com>
2020-04-13 16:46:37 -0700

    add read from file

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ extern crate regex;
 use clap::{App, Arg};
 use regex::Regex;
 
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 fn main() {
     let args = App::new("rusty-search")
         .version("0.1")
@@ -14,22 +18,25 @@ fn main() {
                 .takes_value(true)
                 .required(true),
         )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search")
+                .takes_value(true)
+                .required(true),
+        )
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let quote = "Every face, every shop,
-bedroom window, public-house, and
-dark square is a picture
-feverishly turned--in search of what?
-It is the same with books.
-What do we seek
-through millions of pages?";
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
 
-    for line in quote.lines() {
-        match re.find(line) {
-            Some(_) => println!("{}", line),
+    for (idx, line_) in reader.lines().enumerate() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}: {}", idx + 1, line),
             None => (),
         }
     }
