c27e85658d2440de08a06cb32ce296e89a8bb1de
Edgar Zapeka <edgar.zapeka@gmail.com>
2020-04-13 16:34:45 -0700

    add cli args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+extern crate clap;
 extern crate regex;
 
+use clap::{App, Arg};
 use regex::Regex;
 
 fn main() {
-    let re = Regex::new("picture").unwrap();
+    let args = App::new("rusty-search")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
 
     let quote = "Every face, every shop,
 bedroom window, public-house, and
