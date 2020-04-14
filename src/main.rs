178542708e4727dffce02e42ca94f73a39c477c7
Edgar Zapeka <edgar.zapeka@gmail.com>
2020-04-14 07:30:09 -0700

    add reading from command line

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,19 @@ use clap::{App, Arg};
 use regex::Regex;
 
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
+    }
+}
 
 fn main() {
     let args = App::new("rusty-search")
@@ -29,15 +40,14 @@ fn main() {
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let input = args.value_of("input").unwrap();
-    let f = File::open(input).unwrap();
-    let reader = BufReader::new(f);
-
-    for (idx, line_) in reader.lines().enumerate() {
-        let line = line_.unwrap();
-        match re.find(&line) {
-            Some(_) => println!("{}: {}", idx + 1, line),
-            None => (),
-        }
+    let input = args.value_of("input").unwrap_or("-");
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
     }
 }
